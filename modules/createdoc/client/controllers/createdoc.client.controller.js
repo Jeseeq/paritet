40c7f577748f0d407ec123b1bac9651e8d6da082
Vitalii Hrytsiienko <v.hrytsiienko@gmail.com>
2015-11-28 20:01:25 +0200

    added new menu

--- modules/createdoc/client/controllers/createdoc.client.controller.js
+++ modules/createdoc/client/controllers/createdoc.client.controller.js
@@ -1,7 +1,5 @@
 'use strict';
 
-// Create the 'createdoc' controller
-angular.module('createdoc', ['ngAnimate', 'ui.bootstrap']);
 angular.module('createdoc').controller('CreatedocController', ['$scope',
   function ($scope) {
     $scope.hello = 'Jony';
