b4ffafdd1857f86ee7b714b7cf3aeba10788fdab
Vitalii Hrytsiienko <v.hrytsiienko@gmail.com>
2015-11-28 19:12:02 +0200

    added boilerplate

--- modules/createdoc/client/controllers/createdoc.client.controller.js
+++ modules/createdoc/client/controllers/createdoc.client.controller.js
@@ -10,53 +10,6 @@ angular.module('createdoc').controller('CreatedocController', ['$scope',
     $scope.oneAtATime = false;
     $scope.disable = true;
 
-    $scope.questions = [
-    {
-      id: "1",
-      title: "Роботодавець є:  ",
-      subquestions: [
-
-      {content :"Юридичною особою"}, 
-      {content :"Фізичною особою – підприємцем"}, 
-      {content :"Фізичною особою  "}
-      ] 
-    }
-
-    ,
-    {
-      id: "2",
-      title: "Позов подається Працівником чи його Представником? ",
-      subquestions: [
-
-      {content :"Працівником"}, 
-      {content :"Представником"}
-      ] 
-    }
-    ,
-    {
-      id: "3",
-      title: "Чи укладався письмовий трудовий договір? ",
-      subquestions: [
-
-      {content :"Так"}, 
-      {content :"Ні"}
-      ] 
-    }
-    ,
-    {
-      id: "4",
-      title: "Трудовий договір визначено як:  ",
-      subquestions: [
-
-      {content :"Безстроковий, що укладений на невизначений строк"}, 
-      {content :"Строковий, що укладений на визначений строк, встановлений за погодженням сторін"}, 
-      {content :"Таким, що укладається на час виконання певної роботи"}
-      ] 
-    }
-
-    ]
-  
-
     $scope.disable = function(){
       scope.status.isDisabled = scope.disable;
     };
