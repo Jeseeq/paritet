3f5b5b03e6e4eead08a49dd62efe7955ad8bd3bf
Vitalii Hrytsiienko <v.hrytsiienko@gmail.com>
2015-11-28 19:21:41 +0200

    added boilerplate

--- modules/createdoc/client/controllers/createdoc.client.controller.js
+++ modules/createdoc/client/controllers/createdoc.client.controller.js
@@ -4,20 +4,7 @@
 angular.module('createdoc', ['ngAnimate', 'ui.bootstrap']);
 angular.module('createdoc').controller('CreatedocController', ['$scope',
   function ($scope) {
+    $scope.hello = 'Jony';
 
-    // Create a messages array
-    
-    $scope.oneAtATime = false;
-    $scope.disable = true;
-
-    $scope.disable = function(){
-      $scope.status.isDisabled = $scope.disable;
-    };
-
-    $scope.status = {
-      isFirstOpen: true,
-      isFirstDisabled: false,
-      isDisabled: false
-    };
 }
 ]);
