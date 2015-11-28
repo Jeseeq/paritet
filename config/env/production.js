c480e0579515304b4ad5ba9cfebbfa4adfd86142
U-TOP7\top7 <v.hrytsiienko@gmail.com>
2015-11-28 04:58:59 +0200

    try 4

--- config/env/production.js
+++ config/env/production.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   port: process.env.PORT || 8443,
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
+    uri: process.env.MONGO_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
     options: {
       user: '',
       pass: ''
