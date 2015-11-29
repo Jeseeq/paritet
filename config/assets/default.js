6e7b25340b23f37ca9dc461ae2799e3a96e37e08
Vitalii Hrytsiienko <v.hrytsiienko@gmail.com>
2015-11-29 15:50:29 +0200

    smooth scroll add

--- config/assets/default.js
+++ config/assets/default.js
@@ -16,7 +16,12 @@ module.exports = {
         'public/lib/angular-ui-utils/ui-utils.js',
         'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
         'public/lib/angular-file-upload/angular-file-upload.js',
-        'public/lib/owasp-password-strength-test/owasp-password-strength-test.js'
+        'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
+        'public/lib/jquery/dist/jquery.js',
+        'public/lib//jquery-migrate/jquery-migrate.js',
+        'public/lib/bootstrap/dist/js/bootstrap.min.js',
+        'public/lib/unify/plugins/smoothScroll.js',
+        'public/lib/unify/plugins/back-to-top'
       ],
       tests: ['public/lib/angular-mocks/angular-mocks.js']
     },
