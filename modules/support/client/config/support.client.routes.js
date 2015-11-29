d4105f19512be274753ca3dca6b4669bfde82423
Vitalii Hrytsiienko <v.hrytsiienko@gmail.com>
2015-11-29 23:18:36 +0200

    added support page

--- modules/support/client/config/support.client.routes.js
+++ modules/support/client/config/support.client.routes.js
@@ -1,12 +1,12 @@
 'use strict';
 
-// Configure the 'createdoc' module routes
+// Configure the 'support' module routes
 angular.module('support').config(['$stateProvider',
   function ($stateProvider) {
     $stateProvider
       .state('support', {
         url: '/support',
-        templateUrl: 'modules/createdoc/client/views/support.client.view.html',
+        templateUrl: 'modules/support/client/views/support.client.view.html',
         data: {
           roles: ['user', 'admin']
         }
