bdbe69b2e48a420a417ef22b64a690447dde5e79
Vitalii Hrytsiienko <v.hrytsiienko@gmail.com>
2015-12-15 19:31:32 +0200

    prod mode test

--- modules/createdoc/client/controllers/OpenModals.client.controller.js
+++ modules/createdoc/client/controllers/OpenModals.client.controller.js
@@ -2,10 +2,10 @@
 
 angular.module('createdoc').controller('ModalQuestionThree', ['$scope', '$stateParams','$location','Authentication','documentData', '$log', '$uibModal',
     function ($scope, $stateParams, $location, Authentication, documentData, $log, $uibModal, $q, $http) {
-        $scope.data = documentData;
-        $scope.authentication = Authentication;
-        $scope.person = {
-            first_name: Authentication.user.firstName || '',
-            last_name: Authentication.user.lastName || ''
-        };
+      $scope.data = documentData;
+      $scope.authentication = Authentication;
+      $scope.person = {
+        first_name: Authentication.user.firstName || '',
+        last_name: Authentication.user.lastName || ''
+      };
     }]);
